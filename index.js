aae2421cffcda4b56b0d11e6a22cc948d815259a
songrahardik <songrahardik082@gmail.com>
2025-07-15 11:09:42 +0530

    basic program

--- index.js
+++ index.js
@@ -1,3 +1,64 @@
-const name="Atmiya University"
-console.log("Hello "+ name)
-console.log(`Hello ${name} `)
+// const name="Atmiya University"
+// console.log("Hello "+ name)
+// console.log(`Hello ${name} `)
+
+//1. Even odd number
+// let num = 7;
+// if(num % 2 == 0 )
+// {
+//     console.log("Even Number");
+// }
+// else
+// {
+//     console.log("Odd Number");
+// }
+
+
+//2.Find the Maximum of Two Numbers
+// let a = 5, b = 9;
+// if(a>b)
+// {
+    // console.log("a is maximum");  
+// }
+// else
+// {
+    // console.log("b is maximum");  
+// }
+// 
+
+//3.Check Leap Year
+// let year = 2020;
+// if((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0))
+// {
+    // console.log("Leap Year");
+// }
+// else
+// {
+    // console.log("Not Leap Year");
+// }
+
+//4.Sum of Natural Numbers
+// const j = 5
+// let sum = 0;
+// for(i=1;i<=j;i++)
+// {
+    // sum += i
+    // console.log("i:",i)
+// }
+// console.log(sum)
+
+//5.Factorial of a Number
+// let no = 4;
+// let fact = 1;
+// for (let i = 1; i <= no; i++) {
+    // fact *= i;
+// }
+// console.log(fact);
+
+//6.Multiplication table
+// let num = 3;
+// for (let i = 1; i <= 10; i++) {
+    // console.log(`${num} x ${i} = ${num * i}`);
+// }
+
+// 7.Reverse a Number
